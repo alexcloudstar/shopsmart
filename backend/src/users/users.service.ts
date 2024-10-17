44404068e20ebcb23a09ff341a20f9a59608ec8e
alexcloudstar <alexcloudstar@gmail.com>
2024-10-17 19:44:22 +0300

    feat: complete user registration

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as argon2 from 'argon2';
 import { User } from 'src/models/users/user.entity';
 import { Repository } from 'typeorm';
 
@@ -12,6 +13,18 @@ export class UsersService {
 
   async create(createUserDto: User): Promise<User> {
     try {
+      const user = await this.userRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+
+      if (user) {
+        throw new Error('User already exists');
+      }
+
+      const hashedPw = await argon2.hash(createUserDto.password);
+
+      createUserDto.password = hashedPw;
+
       return this.userRepository.save(createUserDto);
     } catch (error) {
       throw new Error(error);
@@ -19,6 +32,17 @@ export class UsersService {
   }
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.find();
+    try {
+      const users = await this.userRepository.find();
+
+      const mappedUsers = users.map((user) => ({
+        ...user,
+        password: undefined,
+      }));
+
+      return mappedUsers;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
