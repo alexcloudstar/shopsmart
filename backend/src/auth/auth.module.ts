fc2a06a1e07dbdbe7dd04ee33b7bbe75969da3aa
alexcloudstar <alexcloudstar@gmail.com>
2024-10-17 21:48:08 +0300

    cleanup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,12 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/models/users/user.entity';
-import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService],
 })
 export class AuthModule {}
