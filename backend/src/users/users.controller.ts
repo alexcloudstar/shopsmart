a8ca46882411b17f789e81c4707dccca63b379db
alexcloudstar <alexcloudstar@gmail.com>
2024-10-17 22:35:32 +0300

    feat: Protect users routes

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/models/users/user.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
