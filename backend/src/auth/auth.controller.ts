14db28aaaffd4a97b2586aa5989389ad845b79bd
alexcloudstar <alexcloudstar@gmail.com>
2024-10-17 21:48:51 +0300

    feat: using HTTPStatus instead of hardcoded status codes

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { User } from 'src/models/users/user.entity';
 import { UserType } from 'src/users/interfaces/user.interface';
 import { AuthService } from './auth.service';
@@ -8,13 +8,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   async login(@Body() user: User): Promise<Omit<User, 'password'>> {
     return this.authService.login(user.email, user.password);
   }
 
   @Post('/register')
-  @HttpCode(201)
+  @HttpCode(HttpStatus.CREATED)
   async create(@Body() createUser: User): Promise<User> {
     const assignUserType = (type: string): UserType => {
       switch (type) {
