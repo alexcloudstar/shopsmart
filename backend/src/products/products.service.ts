b5904224a1dc50dbdca585e6bf43350d1cc4462b
alexcloudstar <alexcloudstar@gmail.com>
2024-11-19 15:27:40 +0200

    fmt

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { ForbiddenException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from 'src/models/products/product.entity';
 import { Repository } from 'typeorm';
