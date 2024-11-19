9b32602fd7dbdbd3fc795f95b4e26134afbe469d
alexcloudstar <alexcloudstar@gmail.com>
2024-11-19 14:35:56 +0200

    feat: Update User Module to include products

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/models/users/user.entity';
+import { Product } from 'src/models/products/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Product])],
   providers: [UsersService],
   exports: [TypeOrmModule],
 })
